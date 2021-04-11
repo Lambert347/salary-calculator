38c1d5854be6bea855aa626186dc699d1b762de6
Ian Lambert <lambe347@umn.edu>
2021-04-11 18:28:37 -0500

    looks good, needs comments

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 console.log('JAVAJAVAJAVAJAVA');
 
 const employees = [];
-
+let monthlyExpense = 0;
 
 $(document).ready(readyOn);
 
+//function to ready the DOM to be edited
 function readyOn(){
     console.log('JQ');
+    //empties the monthly expenses element in the html, then appends in the value of the variable monthly expense, which is 0
+    let el = $('.monthlyOut');
+    el.empty();
+    el.append(monthlyExpense);
+    //when the submit button is clicked, run the add employee function
     $('#submitButton').on('click', addEmployee);
+    //when the delete button is clicked, run the delete employee function
+    $('.empTableBody').on('click', '.delete', deleteDrone);
+    
     renderEmployees();
+    
 }
 
+//
 function renderEmployees(){
     $('.empTableBody').empty();
 
@@ -22,6 +33,9 @@ function renderEmployees(){
                 <td>${person.number}</td>
                 <td>${person.jobTitle}</td>
                 <td>${person.annualSalary}</td>
+                <td>
+                    <button class ="delete">Delete</button>
+                </td>
             </tr>
         `)  
     }
@@ -35,12 +49,48 @@ function addEmployee(){
         jobTitle: $('#inJobTitle').val(),
         annualSalary: $('#inAnnualSalary').val(),
     }
-    employees.push(newEmployee);
     
-    clearInputFields();
+    for (let i = 0; i < employees.length; i++){
+        if ($('#inNumber').val() === employees[i].number){
+            alert('Employee ID Number must be unique');
+            return 1;
+        }
+
+    }
+
+    if (!$('#inNumber').val()){
+        alert('Please Enter Employee Number');
+    }
+    else {
+        employees.push(newEmployee);
+        clearInputFields();
+    }
+
     
     renderEmployees();
+    calculateExpenses();
 }
+
+function calculateExpenses(){
+    let expense = 0;
+    for (let i = 0; i < employees.length; i++){
+        expense += Number((employees[i].annualSalary)/12); 
+    }
+    let totalExpense = expense + monthlyExpense;
+    let el = $('.monthlyOut');
+    el.empty();
+    el.append(totalExpense);
+    if (totalExpense > 20000){
+        $('.monthlyOut').css("background-color", "red");
+        alert('Monthly Budget Exceeded. Please contact your supervisor.');
+    }
+
+}
+
+function deleteDrone(){
+    $(this).closest('tr').remove();
+}
+
 
 function clearInputFields(){
     $('#inFirstName').val('');
