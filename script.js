771314a8c9b26c8b1bbc10ea33187a5f81217dbb
Ian Lambert <lambe347@umn.edu>
2021-04-11 18:34:59 -0500

    comments done, ready to submit

--- script.js
+++ script.js
@@ -21,10 +21,11 @@ function readyOn(){
     
 }
 
-//
+//function to render the employees to the DOM
 function renderEmployees(){
+    //empties the table prior to adding the new employee
     $('.empTableBody').empty();
-
+    //loops through the employee array and appends the information to their respective cells in the table on the DOM
     for (let person of employees){
         $('.empTableBody').append(`
             <tr>
@@ -40,7 +41,7 @@ function renderEmployees(){
         `)  
     }
 }
-
+//function to add employees to the employee array
 function addEmployee(){
     let newEmployee = {
         firstName: $('#inFirstName').val(),
@@ -49,7 +50,7 @@ function addEmployee(){
         jobTitle: $('#inJobTitle').val(),
         annualSalary: $('#inAnnualSalary').val(),
     }
-    
+    //loop to check if the employee id number is unique, if not, it alerts the user and returns an error
     for (let i = 0; i < employees.length; i++){
         if ($('#inNumber').val() === employees[i].number){
             alert('Employee ID Number must be unique');
@@ -57,41 +58,47 @@ function addEmployee(){
         }
 
     }
-
+    //checks if there is an employee number, if not, alerts the user
     if (!$('#inNumber').val()){
         alert('Please Enter Employee Number');
     }
+    //if the above two conditions check out, then the employee is added to the array and then the input fields are cleared.
     else {
         employees.push(newEmployee);
         clearInputFields();
     }
 
-    
+    //runs the renderEmployees function
     renderEmployees();
+    //runs the calculateExpenses fuction
     calculateExpenses();
 }
-
+//function to calculate the new expenses with each new employee added
 function calculateExpenses(){
     let expense = 0;
+    //loop to move through the employee array and add the annual salary divided by 12 to the monthly expenses 
     for (let i = 0; i < employees.length; i++){
         expense += Number((employees[i].annualSalary)/12); 
     }
+    //adds the expenses together
     let totalExpense = expense + monthlyExpense;
+    //empties the expenses on the DOM, then appends in the total expenses to the monthly expenses on the DOM
     let el = $('.monthlyOut');
     el.empty();
     el.append(totalExpense);
+    //if the monthly expenses exceeds 20000, then the expenses turns red and the user is alerted.
     if (totalExpense > 20000){
         $('.monthlyOut').css("background-color", "red");
         alert('Monthly Budget Exceeded. Please contact your supervisor.');
     }
 
 }
-
+//function to delete an employee from the DOM
 function deleteDrone(){
     $(this).closest('tr').remove();
 }
 
-
+//function to clear the input fields
 function clearInputFields(){
     $('#inFirstName').val('');
     $('#inLastName').val('');
